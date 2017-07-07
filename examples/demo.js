5d7c3d3bb709f3b160b8e1eb44134cc966e5024e
xiaowei hu <billhu422@126.com>
2017-07-07 09:33:38 +0000

    add examples and add mysql pachages into package.json

---
+++ examples/demo.js
@@ -0,0 +1,56 @@
+var Sequelize = require('sequelize'),
+    epilogue = require('../../epilogue'),
+    http = require('http');
+
+// Define your models
+var database = new Sequelize('inventory', 'root', 'qaz123',{
+	host:'192.168.87.152'
+});
+var User = database.define('User', {
+  username: Sequelize.STRING,
+  birthday: Sequelize.DATE
+});
+
+// Initialize server
+var server, app;
+if (process.env.USE_RESTIFY) {
+  var restify = require('restify');
+
+  app = server = restify.createServer()
+  app.use(restify.queryParser());
+  app.use(restify.bodyParser());
+} else {
+  var express = require('express'),
+      bodyParser = require('body-parser');
+
+  var app = express();
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  server = http.createServer(app);
+}
+
+// Initialize epilogue
+epilogue.initialize({
+  app: app,
+  sequelize: database,
+  updateMethod: 'put'
+});
+
+// Create REST resource
+var userResource = epilogue.resource({
+  model: User,
+  endpoints: ['/users', '/users/:id']
+});
+
+// Create database and listen
+database
+//  .sync({ force: true })
+  .sync({ force: false })
+  .then(function() {
+    server.listen(80,function() {
+      var host = server.address().address,
+          port = server.address().port;
+
+      console.log('listening at http://%s:%s', host, port);
+    });
+  });
